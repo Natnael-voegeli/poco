163c1844db641cc4b583b29d95bfd7dc04d632ac
Natnael Voegeli <natawi21t@gmail.com>
2019-11-18 12:44:47 +0100

    updating for mac

---
+++ workshop/PHP-SQL/mini-site/v2-PHP/syntax.php
@@ -0,0 +1,25 @@
+<?php
+// Number
+$bob_geldof = 3;
+
+// String
+$bob = "Bob is great !";
+
+// Array
+$bob = [4,5,6];
+
+// Booleans
+$bob = true;
+$bob = false;
+
+// Functions
+function bob($birthYear) {
+
+$age = 2019 - $birthYear;
+
+return "Bob is "  . $age;
+
+    return "BOB !!!";
+}
+echo bob(1995); 
+
